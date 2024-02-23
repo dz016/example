2f0c96a37f7e58689f5d4bae787111a2f6304941
Dawood Zargar <dawoodzargar08@gmail.com>
2024-02-23 20:27:21 +0530

    sfbysdfvsl

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
-// Connect to MongoDB
+//dshbfkubfadsufvskvsd sjyvsd
 // DONT MISUSE THIS THANKYOU!!
 // mongoose.connect("mongodb://localhost:27017/courses", {
 //   useNewUrlParser: true,
